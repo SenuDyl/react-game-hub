4276795620304c1fd9dd34cf56d773740d03acb0
iamsenuri <senurimallikarachchi@gmail.com>
2024-06-23 13:53:51 +0530

    Theme added

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider, ColorModeScript } from '@chakra-ui/react'
 import './index.css';
 import App from './App';
+import theme from './theme'
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ChakraProvider><App /></ChakraProvider>
+    <ChakraProvider theme={theme}>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+
+      <App /></ChakraProvider>
 
   </React.StrictMode>
 );
